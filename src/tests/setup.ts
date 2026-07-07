e8643ecd56899b3e5753d7cca9ab79e7208c43d2
barsbold-coding <barsbold.coding@gmail.com>
2026-07-07 17:55:41 +0800

    Add jsdom polyfills for component test setup

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom'
+import { cleanup } from '@testing-library/react'
+import { afterEach } from 'vitest'
+
+afterEach(() => cleanup())
+
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+// jsdom doesn't implement the Pointer Events API that Radix Select relies on.
+Element.prototype.hasPointerCapture ??= () => false
+Element.prototype.setPointerCapture ??= () => {}
+Element.prototype.releasePointerCapture ??= () => {}
+Element.prototype.scrollIntoView ??= () => {}
